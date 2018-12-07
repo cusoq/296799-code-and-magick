5612ec03c7940cee5a1c667739c819a9b04c6c20
cusoq <cusoq@mail.ru>
2018-12-07 16:32:07 +0400

    Set form upload (xhr), load remote data (xhr), error processing and readout

--- js/setup.js
+++ js/setup.js
@@ -7,7 +7,6 @@
   var wizards = [];
   var similarListElement = document.querySelector('.setup-similar-list');
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-  var fragment = document.createDocumentFragment();
   // имя и фамилия волшебника
   var getFullName = function () {
     return WIZARD_NAMES[window.util.getRandom(WIZARD_NAMES.length)] + ' ' + WIZARD_SURNAMES[window.util.getRandom(WIZARD_SURNAMES.length)];
@@ -16,8 +15,8 @@
   for (var j = 0; j < WIZARDS_QUANTITY; j++) {
     wizards[j] = {
       name: getFullName(),
-      coatColor: window.util.getColor(window.util.COAT_COLORS),
-      eyesColor: window.util.getColor(window.util.EYES_COLORS)
+      colorCoat: window.util.getColor(window.util.COAT_COLORS),
+      colorEyes: window.util.getColor(window.util.EYES_COLORS)
     };
   }
   // ДОМ структура х-к персонажа
@@ -27,14 +26,30 @@
   var renderWizard = function (currentWizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
     wizardElement.querySelector('.setup-similar-label').textContent = currentWizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = currentWizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = currentWizard.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = currentWizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = currentWizard.colorEyes;
     return wizardElement;
   };
-  // вставка сформированных ДОМ элементов в разметку
-  for (var i = 0; i < wizards.length; i++) {
-    fragment.appendChild(renderWizard(wizards[i]));
-  }
-  similarListElement.appendChild(fragment);
-  document.querySelector('.setup-similar').classList.remove('hidden');
+  // Обработка успешной загрузки данных по сети
+  var onSuccessLoad = function (xhrResponseWizards) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < 4; i++) {
+      fragment.appendChild(renderWizard(xhrResponseWizards[i]));
+    }
+    similarListElement.appendChild(fragment);
+    document.querySelector('.setup-similar').classList.remove('hidden');
+  };
+  // Обработка возможных ошибок
+  var onErrorLoad = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+  // вставка сформированных ДОМ элементов в разметку или вывод сообщения об ошибке
+  window.backend.load(onSuccessLoad, onErrorLoad);
 })();
